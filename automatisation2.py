33376b779fcbbe9f04cbb941ef0391d866380ae1
Aurelien <aurelien.benoit@telecomnancy.net>
2018-04-16 18:02:35 +0200

    Tests en fonctions de différents paramétres, en regroupant les classes

--- automatisation2.py
+++ automatisation2.py
@@ -7,14 +7,18 @@ from sklearn import svm
 from sklearn.metrics import classification_report
 import sklearn.metrics as metrics
 
-random_state = 42 # On gare toujours la même graine pou les tests.
+random_state = 42  # On gare toujours la même graine pou les tests.
 
 class MachineLearn():
   """[summary]
     Wrapper du proccessus de macine learning
   """
 
-  def __init__(self, test_well_name: str = "SHANKLE", features: list = ['GR', 'ILD_log10', 'DeltaPHI', 'PHIND', 'PE', 'NM_M', 'RELPOS']):
+  def __init__(self,
+     test_well_name: str = "SHANKLE",
+      features: list = ['GR', 'ILD_log10', 'DeltaPHI', 'PHIND', 'PE', 'NM_M', 'RELPOS'],
+      facies_group: list = [1,2,3,4,5,6,7,8]
+      ):
     """[summary]
     
     Keyword Arguments:
@@ -27,9 +31,23 @@ class MachineLearn():
     self.data: pd.DataFrame = pd.read_csv('resources/training_dat.csv')
     self.features = features
 
-    # Decoupage des donnees en un dataframe d'aprentissage et un autre de test  
+    #print(self.data['Facies'])
+
+    # Regouprer les facies
+    self.data.Facies.replace([1, 2, 3, 4, 5, 6, 7, 8],
+     [int(facies_group[0]),
+      int(facies_group[1]),
+      int(facies_group[2]),
+      int(facies_group[3]),
+      int(facies_group[4]),
+      int(facies_group[5]),
+      int(facies_group[6]),
+      int(facies_group[7])], inplace=True)
+
+    # Decoupage des donnees en un dataframe d'aprentissage et un autre de test
     self.learn_data = self.data[self.data['Well Name'] != test_well_name]
-    self.test_well_data = self.data[self.data['Well Name'] == test_well_name] #Well = puit -> nom du puit, pour tester le machine learning
+    # Well = puit -> nom du puit, pour tester le machine learning
+    self.test_well_data = self.data[self.data['Well Name'] == test_well_name]
 
     # Preparation des vecteurs d'apprentissage/de test
     self.learn_features_vector = self.learn_data[features]
@@ -38,35 +56,37 @@ class MachineLearn():
 
     # Scaler les vecteurs
     scaler = StandardScaler().fit(self.learn_features_vector)
-    self.learn_scaled_features = scaler.transform(self.learn_features_vector) 
-    self.test_well_scaled_features = scaler.transform(self.test_well_features_vector)
+    self.learn_scaled_features = scaler.transform(self.learn_features_vector)
+    self.test_well_scaled_features = scaler.transform(
+        self.test_well_features_vector)
 
-    # 
+    #
     self.y_test_well = self.test_well_data["Facies"]
 
   def show_plot(self):
     """Affiche le plot des paramètres
     """
 
-    sns.pairplot(self.learn_features_vector[['GR', 'ILD_log10', 'DeltaPHI', 'PHIND', 'PE']])
+    sns.pairplot(self.learn_features_vector[[
+                 'GR', 'ILD_log10', 'DeltaPHI', 'PHIND', 'PE']])
     plt.show()
 
   def test(self,
-      C:int = 10,
-      kernel: str = 'rbf',
-      degree: int = 3,
-      gamma: int = 1,
-      coef0: float = 0.0,
-      shrinking: bool=True,
-      probability: bool=False,
-      tol: float =0.001,
-      cache_size=200,
-      class_weight=None,
-      verbose=False,
-      max_iter=-1,
-      decision_function_shape='ovr',
-      random_state=None,
-      verbose_report=False):
+           C: int = 10,
+           kernel: str = 'rbf',
+           degree: int = 3,
+           gamma: int = 1,
+           coef0: float = 0.0,
+           shrinking: bool=True,
+           probability: bool=False,
+           tol: float =0.001,
+           cache_size=200,
+           class_weight=None,
+           verbose=False,
+           max_iter=-1,
+           decision_function_shape='ovr',
+           random_state=None,
+           verbose_report=False):
     """Pour tester les paramètres sans tout réécrire à chaque fois
     
     Keyword Arguments:
@@ -87,7 +107,6 @@ class MachineLearn():
       verbose_report {bool} -- Affichage ou non du report (default: True)
     """
 
-
     # Préparer la cross-validation
     """
     x_train, x_cv, y_train, y_cv = train_test_split(
@@ -103,19 +122,19 @@ class MachineLearn():
 
     # Paramétrage de la machine
     clf = svm.SVC(
-      C=C, 
-      kernel=kernel,
-      gamma=gamma,
-      shrinking=shrinking,
-      probability=probability,
-      tol=tol,
-      cache_size=cache_size,
-      class_weight=class_weight,
-      verbose=verbose,
-      max_iter=max_iter,
-      decision_function_shape=decision_function_shape,
-      random_state=random_state
-      )
+        C=C,
+        kernel=kernel,
+        gamma=gamma,
+        shrinking=shrinking,
+        probability=probability,
+        tol=tol,
+        cache_size=cache_size,
+        class_weight=class_weight,
+        verbose=verbose,
+        max_iter=max_iter,
+        decision_function_shape=decision_function_shape,
+        random_state=random_state
+    )
     # Apprentissage
     clf.fit(x_train, y_train)
 
@@ -123,10 +142,10 @@ class MachineLearn():
     y_pred = clf.predict(self.test_well_scaled_features)
 
     test_well_data_with_pred = self.test_well_data
-    test_well_data_with_pred.loc[:,"Prediction"] = y_pred
+    test_well_data_with_pred.loc[:, "Prediction"] = y_pred
 
     #target_names = ['SS', 'CSiS', 'FSiS', 'SiSh', 'MS', 'WS', 'D', 'PS', 'BS']
-    
+
     #report = classification_report(self.y_test_well, y_pred, target_names=target_names)
     accuracy_score = metrics.accuracy_score(self.y_test_well, y_pred)
     if (verbose_report):
@@ -135,70 +154,66 @@ class MachineLearn():
 
 ### FIN CLASSE ###
 
+
 if __name__ == '__main__':
+
+  #TODO Factoriser
+
+  facies_group = [1,1,2,2,2,3,3,3] # Si'lon veut regrouper les facies
+
   mach = MachineLearn(
-      features = ['GR', 'ILD_log10', 'DeltaPHI','PHIND', 'PE', 'NM_M', 'RELPOS']
+      features=['GR', 'ILD_log10', 'DeltaPHI', 'PHIND', 'PE', 'NM_M', 'RELPOS'],
+      facies_group = facies_group
   )
   mach2 = MachineLearn(
-      features=['GR', 'ILD_log10', 'DeltaPHI', 'PHIND', 'PE', 'RELPOS'])
+      features=['GR', 'ILD_log10', 'DeltaPHI', 'PHIND', 'PE', 'RELPOS'],
+      facies_group=facies_group)
   mach3 = MachineLearn(
-      features=['GR', 'ILD_log10', 'DeltaPHI', 'PHIND', 'PE', 'NM_M'])
+      features=['GR', 'ILD_log10', 'DeltaPHI', 'PHIND', 'PE', 'NM_M'],
+      facies_group=facies_group)
   mach4 = MachineLearn(
-      features=['GR', 'ILD_log10', 'DeltaPHI', 'PHIND', 'PE'])
+      features=['GR', 'ILD_log10', 'DeltaPHI', 'PHIND', 'PE'],
+      facies_group=facies_group)
   mach_sans_GR = MachineLearn(
-      features=['ILD_log10', 'DeltaPHI', 'PHIND', 'PE', 'RELPOS', 'NM_M']
+      features=['ILD_log10', 'DeltaPHI', 'PHIND', 'PE', 'RELPOS', 'NM_M'],
+      facies_group=facies_group
   )
   mach_sans_ILD_log10 = MachineLearn(
-    features=['GR', 'DeltaPHI', 'PHIND', 'PE', 'RELPOS', 'NM_M']
+      features=['GR', 'DeltaPHI', 'PHIND', 'PE', 'RELPOS', 'NM_M'],
+      facies_group=facies_group
   )
   mach_sans_DeltaPHI = MachineLearn(
-      features=['GR', 'ILD_log10', 'PHIND', 'PE', 'NM_M', 'RELPOS']
+      features=['GR', 'ILD_log10', 'PHIND', 'PE', 'NM_M', 'RELPOS'],
+      facies_group=facies_group
   )
   mach_sans_PHIND = MachineLearn(
-      features=['GR', 'ILD_log10', 'DeltaPHI', 'PE', 'NM_M', 'RELPOS']
+      features=['GR', 'ILD_log10', 'DeltaPHI', 'PE', 'NM_M', 'RELPOS'],facies_group=facies_group
   )
   mach_sans_PE = MachineLearn(
-      features=['GR', 'ILD_log10', 'DeltaPHI', 'NM_M', 'RELPOS']
+      features=['GR', 'ILD_log10', 'DeltaPHI', 'NM_M', 'RELPOS'],
+      facies_group=facies_group
   )
 
   #mach.test(gamma="auto")
 
-  #TODO Faire des tests itératifs sur l'influence d'un seul paramètre
-  
   results = list()
 
-  #print("== Test originel ==")
   results.append(["Originel", mach.test()])
-  #print("\n== Test sans NM_M ==")
   results.append(["Sans NM_M", mach2.test()])
-  #print("\n== Test sans RELPOS ==") #RELPOS par rapport à une position de référence
   results.append(["Sans RELPOS", mach3.test()])
-  #print("\n== Test sans NM_M et RELPOS ==")
   results.append(["Sans GR", mach4.test()])
-  #print("\n== Test sans GR ==")
   results.append(["Sans GR", mach_sans_GR.test()])
-  #print("\n== Test sans ILD_log10 ==")
   results.append(["Sans ILD_log10", mach_sans_ILD_log10.test()])
-  #print("\n== Test sans DeltaPHI ==")
   results.append(["Sans DeltaPHI", mach_sans_DeltaPHI.test()])
-  #print("\n== Test sans PHIND ==")
   results.append(["Sans PHIND", mach_sans_PHIND.test()])
-  #print("\n== Test sans PE ==")
   results.append(["Sans PE", mach_sans_PE.test()])
 
   print("RESULTS")
-  for i in results :
-    print(i[0], "{", "accuracy :", i[1], "| delta_accuracy :", i[1] - results[0][1], "}")
+  print("For facies =", facies_group)
+  for i in results:
+    print(i[0], "{", "accuracy :", i[1],
+          "| delta_accuracy :", i[1] - results[0][1], "}")
 
 
-  # Amélioration lorsque l'on retire la resistivité, peut être expliquée par la différence des fluides, par la resistivité 
+  # Amélioration lorsque l'on retire la resistivité, peut être expliquée par la différence des fluides, par la resistivité
   # variable au sein d'un même facies (la résitance est très différente en fonction de la position de la mesure dans la formation géologique)
-
-
-
-
-  
-
-
-
-    
